24e4abda9cbf0103a0e7e81c0d2a4f33f404a637
duellal <alexandriaduell@gmail.com>
2022-08-28 18:43:31 -0400

    Started the Show testing - need to finish

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,12 +3,47 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
-test('renders without errors', () => { });
+const mockShowData = {
+   name: 'Stranger Things',
+   summary: 'Test Summary',
+   seasons: [{
+      id: 1,
+      name: '1',
+      episodes: [],
+   },
+   {
+      id: 2,
+      name: '2',
+      episodes: [],
+   },
+   {
+      id: 3,
+      name: '3',
+      episodes: [],
+   },
+   {
+      id: 4,
+      name: '4',
+      episodes: [],
+   }]
+}
 
-test('renders Loading component when prop show is null', () => { });
+test('renders without errors', () => {
+   render(<Show show={mockShowData} selectedSeason={'none'} />)
+});
 
-test('renders same number of options seasons are passed in', () => { });
+test('renders Loading component when prop show is null', () => {
+   render(<Show show={null} selectedSeason={'none'} />)
+});
 
-test('handleSelect is called when an season is selected', () => { });
+test('renders same number of options seasons are passed in', () => {
+   render(<Show show={mockShowData} selectedSeason={'none'} />)
+});
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
+test('handleSelect is called when an season is selected', () => {
+   render(<Show show={mockShowData} selectedSeason={'none'} />)
+});
+
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+   render(<Show show={mockShowData} selectedSeason={'none'} />)
+});
