64839467bd075f7f36bda6ca6c9e6285849d7689
duellal <alexandriaduell@gmail.com>
2022-08-29 00:44:46 -0400

    On show.test.js - handleSelect test userEvent.selectOptions is not selecting an option

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event'
+
 import Show from './../Show';
 
 const mockShowData = {
@@ -34,14 +36,41 @@ test('renders without errors', () => {
 
 test('renders Loading component when prop show is null', () => {
    render(<Show show={null} selectedSeason={'none'} />)
+
+   const loading = screen.getByTestId(/loading-container/i)
+
+   expect(loading).toBeInTheDocument()
 });
 
 test('renders same number of options seasons are passed in', () => {
    render(<Show show={mockShowData} selectedSeason={'none'} />)
+
+   const numSeasons = screen.getAllByTestId(/season-option/i)
+
+   expect(numSeasons).toHaveLength(4)
 });
 
 test('handleSelect is called when an season is selected', () => {
-   render(<Show show={mockShowData} selectedSeason={'none'} />)
+   const fakeHandleSelect = jest.fn()
+
+   render(<Show show={mockShowData} selectedSeason={'none'} onChange={fakeHandleSelect} />)
+
+   //Sanity Tests - Making sure these are to be expected:
+   expect(screen.getByRole('option', { name: '' }).selected).toBe(true)
+
+   expect(screen.getAllByRole('option').length).toBe(5)
+
+   //Select event is not working - when doing the expect(screen.getByRole('option', { name: '3' }).selected).toBe(true) comes back as false instead of true
+   userEvent.selectOptions(screen.getByRole('combobox'), screen.getByRole('option', { name: '3' }))
+
+   // console.log('HERE!', screen.getByRole('option', { name: '1' }))
+   // expect(screen.getByRole('option', { name: '3' }).selected).toBe(true)
+   // expect(fakeHandleSelect).toHaveBeenCalled()
+
+   expect(screen.getByRole('option', { name: '1' }).selected).toBe(false)
+   expect(screen.getByRole('option', { name: '2' }).selected).toBe(false)
+   expect(screen.getByRole('option', { name: '3' }).selected).toBe(true)
+   expect(screen.getByRole('option', { name: '4' }).selected).toBe(false)
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
